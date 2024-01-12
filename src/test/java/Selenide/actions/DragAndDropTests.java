ada56ef36fb335737ba00113e5f6b61370152dc3
Marina Ostapenko <meostapenko@yandex.ru>
2024-01-12 18:58:22 +0300

    HW6

---
+++ src/test/java/Selenide/actions/DragAndDropTests.java
@@ -0,0 +1,33 @@
+package Selenide.actions;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.actions;
+
+
+public class DragAndDropTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com/drag_and_drop";
+    }
+    @Test
+        void dragAndDropTests() {
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDropTo("column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+        }
+
+    @Test
+    void actionsTests(){
+        actions().moveToElement($("#column-a")).clickAndHold().moveToElement
+                ($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+    }
---
+++ src/test/java/github/SolutionsTests.java
@@ -0,0 +1,24 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.conditions.Text;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+public class SolutionsTests {
+    @BeforeAll
+    static void beforeAll() {
+                Configuration.baseUrl = "https://github.com/";
+    }
+    @Test
+    void shouldFindSolutions() {
+       $$("HeaderMenu-item").filterBy(text("Solutions")).first().hover();
+        $("hero-section-brand-heading").$(byText("The AI-powered\n" +
+                "developer platform.")).click();
+        $("hero-section-brand-heading").shouldHave(text("The AI-powered\n" +
+                "developer platform."));
+    }
+}
